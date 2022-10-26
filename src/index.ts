2719120f3dc42e446d9a0174f708c7ef5e223a66
antooni <antoni.pawlak@pm.me>
2022-10-26 18:39:13 +0200

    working

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,92 @@ window.onkeydown = function (e: KeyboardEvent) {
   pressedKey[e.keyCode] = true
 }
 
+let yaw = -90 //obrót względem osi X
+let pitch = 0 //obrót względem osi
+let licznik = 0
+const fpsElem = document.querySelector('#fps')
+let startTime = 0
+let elapsedTime = 0
+// @ts-expect-error:
+const ustaw_kamere_mysz = (e) => {
+  let xoffset = e.movementX
+  let yoffset = e.movementY
+  let sensitivity = 0.1
+  let cameraSpeed = 0.05 * elapsedTime
+  xoffset *= sensitivity
+  yoffset *= sensitivity
+
+  yaw += xoffset * cameraSpeed
+  pitch -= yoffset * cameraSpeed
+
+  if (pitch > 89.0) pitch = 89.0
+  if (pitch < -89.0) pitch = -89.0
+
+  let front = GLM.vec3.fromValues(1, 1, 1)
+
+  front[0] =
+    Math.cos(GLM.glMatrix.toRadian(yaw)) *
+    Math.cos(GLM.glMatrix.toRadian(pitch))
+  front[1] = Math.sin(GLM.glMatrix.toRadian(pitch))
+  front[2] =
+    Math.sin(GLM.glMatrix.toRadian(yaw)) *
+    Math.cos(GLM.glMatrix.toRadian(pitch))
+  GLM.vec3.normalize(cam_front, front)
+}
+
 const cam_speed = 0.02
-const cam_pos = GLM.vec3.fromValues(0, 0, 3)
-const cam_front = GLM.vec3.fromValues(0, 0, -1)
+let cam_pos = GLM.vec3.fromValues(0, 0, 3)
+let cam_front = GLM.vec3.fromValues(0, 0, -1)
 const cam_up = GLM.vec3.fromValues(0, 1, 0)
 let cam_rot = 0.0
 
-const gl = setupGL()
+function ustaw_kamere() {
+  let cameraSpeed = 0.005 * elapsedTime
+  let cam_front_tmp = GLM.vec3.fromValues(1, 1, 1)
+  if (pressedKey['38']) {
+    //Up
+    cam_pos[0] += cameraSpeed * cam_front[0]
+    cam_pos[1] += cameraSpeed * cam_front[1]
+    cam_pos[2] += cameraSpeed * cam_front[2]
+  }
+  if (pressedKey['40']) {
+    //down
+    cam_pos[0] -= cameraSpeed * cam_front[0]
+    cam_pos[1] -= cameraSpeed * cam_front[1]
+    cam_pos[2] -= cameraSpeed * cam_front[2]
+  }
+  if (pressedKey['37']) {
+    //left
+    let cam_pos_temp= GLM.vec3.fromValues(1, 1, 1)
+    let cross= GLM.vec3.fromValues(1, 1, 1)
+    GLM.vec3.cross(cross, cam_front, cam_up)
+    GLM.vec3.normalize(cam_pos_temp, cross)
+    cam_pos[0] -= cam_pos_temp[0] * cameraSpeed
+    cam_pos[1] -= cam_pos_temp[1] * cameraSpeed
+    cam_pos[2] -= cam_pos_temp[2] * cameraSpeed
+  }
+
+  if (pressedKey['39']) {
+    //right
+    let cam_pos_temp= GLM.vec3.fromValues(1, 1, 1)
+    let cross= GLM.vec3.fromValues(1, 1, 1)
+    GLM.vec3.cross(cross, cam_front, cam_up)
+    GLM.vec3.normalize(cam_pos_temp, cross)
+    cam_pos[0] += cam_pos_temp[0] * cameraSpeed
+    cam_pos[1] += cam_pos_temp[1] * cameraSpeed
+    cam_pos[2] += cam_pos_temp[2] * cameraSpeed
+  }
+
+  let uniView = gl.getUniformLocation(program, 'view')
+  gl.uniformMatrix4fv(uniView, false, view)
+
+  cam_front_tmp[0] = cam_pos[0] + cam_front[0]
+  cam_front_tmp[1] = cam_pos[1] + cam_front[1]
+  cam_front_tmp[2] = cam_pos[2] + cam_front[2]
+  GLM.mat4.lookAt(view, cam_pos, cam_front_tmp, cam_up)
+}
+
+const gl = setupGL(ustaw_kamere_mysz)
 bindBuffer(gl, KOSTKA)
 const program = setupProgram(gl)
 
@@ -35,38 +114,20 @@ const model = createModel()
 function draw() {
   clear(gl)
 
-  if (pressedKey['37']) {
-    // Left
-    cam_rot -= cam_speed
-    cam_front[0] = Math.sin(cam_rot)
-    cam_front[2] = -Math.cos(cam_rot)
-    var cam_front_tmp = GLM.vec3.create()
-    GLM.vec3.add(cam_front_tmp, cam_pos, cam_front)
-    GLM.mat4.lookAt(view, cam_pos, cam_front_tmp, cam_up)
+  elapsedTime = performance.now() - startTime
+
+  startTime = performance.now()
+
+  licznik++
+  let ffps = 1000 / elapsedTime
+
+  if (licznik > ffps) {
+    fpsElem.textContent = ffps.toFixed(1)
+
+    licznik = 0
   }
-  if (pressedKey['38']) {
-    // Up
-    GLM.vec3.scaleAndAdd(cam_pos, cam_pos, cam_front, cam_speed)
-    var cam_front_tmp = GLM.vec3.create()
-    GLM.vec3.add(cam_front_tmp, cam_pos, cam_front)
-    GLM.mat4.lookAt(view, cam_pos, cam_front_tmp, cam_up)
-  }
-  if (pressedKey['39']) {
-    // Right
-    cam_rot += cam_speed
-    cam_front[0] = Math.sin(cam_rot)
-    cam_front[2] = -Math.cos(cam_rot)
-    var cam_front_tmp = GLM.vec3.create()
-    GLM.vec3.add(cam_front_tmp, cam_pos, cam_front)
-    GLM.mat4.lookAt(view, cam_pos, cam_front_tmp, cam_up)
-  }
-  if (pressedKey['40']) {
-    // Down
-    GLM.vec3.scaleAndAdd(cam_pos, cam_pos, cam_front, -cam_speed)
-    var cam_front_tmp = GLM.vec3.create()
-    GLM.vec3.add(cam_front_tmp, cam_pos, cam_front)
-    GLM.mat4.lookAt(view, cam_pos, cam_front_tmp, cam_up)
-  }
+
+  ustaw_kamere()
 
   setupModel(gl, program, model)
   setupView(gl, program, view)
--- src/setupGL.ts
+++ src/setupGL.ts
@@ -1,4 +1,6 @@
-export const setupGL = (): WebGLRenderingContextStrict => {
+  // @ts-expect-error:
+
+export const setupGL = (ustaw_kamere_mysz): WebGLRenderingContextStrict => {
   console.log('This is working')
 
   const canvas = document.getElementById('game-surface')
@@ -14,6 +16,31 @@ export const setupGL = (): WebGLRenderingContextStrict => {
   if (!gl) {
     alert('Your browser does not support WebGL')
   }
+
+  //*****************pointer lock object forking for cross browser**********************
+  canvas.requestPointerLock =
+    canvas.requestPointerLock || canvas.requestPointerLock
+  document.exitPointerLock =
+    document.exitPointerLock || document.exitPointerLock
+  canvas.onclick = function () {
+    canvas.requestPointerLock()
+  }
+  // Hook pointer lock state change events for different browsers
+  document.addEventListener('pointerlockchange', lockChangeAlert, false)
+  document.addEventListener('mozpointerlockchange', lockChangeAlert, false)
+  function lockChangeAlert() {
+    if (
+      document.pointerLockElement === canvas ||
+      document.pointerLockElement === canvas
+    ) {
+      console.log('The pointer lock status is now locked')
+      document.addEventListener('mousemove', ustaw_kamere_mysz, false)
+    } else {
+      console.log('The pointer lock status is now unlocked')
+      document.removeEventListener('mousemove', ustaw_kamere_mysz, false)
+    }
+  }
+  //****************************************************************
 
   return gl
 }
